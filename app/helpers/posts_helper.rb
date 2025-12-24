7ccd98b8e949036c51845bf5ee673acadcd0ce89
Alvin Lin <alvinch.lin@gmail.com>
2025-12-23 17:54:19 -0800

    Render post body as markdown
    Add commonmarker gem.
    Add basic css classes for markdown stylings
    Add markdown rendering helper to render markdown in post body.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,20 @@
 module PostsHelper
+  def render_markdown(text)
+    return "" if text.blank?
+
+    html = Commonmarker.to_html(
+        text,
+        options: {
+          parse: {
+            smart: true
+          },
+          extension: {
+            strikethrough: true,
+            table: true,
+            autolink: true
+          }
+        },
+      )
+    sanitize(html)
+  end
 end
