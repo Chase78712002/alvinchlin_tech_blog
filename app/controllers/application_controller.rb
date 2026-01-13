27f63e549ad6cd3b4d3550fdd9c45277384f1bc2
Alvin Lin <alvinch.lin@gmail.com>
2026-01-12 16:44:22 -0800

    Add basic admin authentication
    Add basic authentication for admin user to prevent other users to write
    access to posts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def require_admin!
+    return unless Rails.env.production?
+
+
+    authenticate_or_request_with_http_basic do |user, password|
+      ActiveSupport::SecurityUtils.secure_compare(user, ENV.fetch("ADMIN_USER")) &
+      ActiveSupport::SecurityUtils.secure_compare(password, ENV.fetch("ADMIN_PASSWORD"))
+    end
+  end
 end
