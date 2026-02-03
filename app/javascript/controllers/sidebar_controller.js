7fb71c5dbabbec0c7ffb870744d3460fb98d71d5
Alvin Lin <alvinch.lin@gmail.com>
2026-02-02 17:15:26 -0800

    Add: Delays to close the sidebar
    Mouse leave triggers timeout to close the sidebar.
    Mouse enter cancels the timeout.
    Refactor the overall logics and namings to improve readability.

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -4,29 +4,48 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["sidebar", "hint"];
   connect() {
-    console.log("Sidebar Controller connected!");
-    console.log("Has sidebar target?:", this.hasSidebarTarget);
-    console.log("Has hint target?:", this.hasHintTarget);
     if (window.innerWidth >= 768) {
       this.sidebarTarget.style.transform = "translateX(-100%)";
       this.sidebarTarget.classList.add("fixed", "z-50", "h-screen");
     }
+    this.closeTimeout = null;
+    this.isOpen = false;
   }
 
-  mouseMove(event) {
-    console.log("Mouse moved to X", event.clientX);
+  trackAndOpen(event) {
     if (window.innerWidth < 768) return;
 
     if (event.clientX <= 50) this.open();
   }
+  scheduleCloseTimer() {
+    this.closeTimeout = setTimeout(() => {
+      this.close();
+    }, 500);
+  }
 
+  keepSidebarOpen() {
+    if (window.innerWidth < 768) return;
+    this.cancelCloseTimer();
+  }
+
+  cancelCloseTimer() {
+    if (this.closeTimeout) {
+      clearTimeout(this.closeTimeout);
+      this.closeTimeout = null;
+    }
+  }
   open() {
-    console.log("open() called");
+    if (this.isOpen) return;
+    this.isOpen = true;
     this.sidebarTarget.style.transform = "translateX(0)";
   }
 
   close() {
-    console.log("close() called");
+    this.isOpen = false;
     this.sidebarTarget.style.transform = "translateX(-100%)";
+  }
+
+  disconect() {
+    this.cancelCloseTimer();
   }
 }
