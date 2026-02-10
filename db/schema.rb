8a9ea3897b3ce6170565f7c19a5872f3b844ce74
Alvin Lin <alvinch.lin@gmail.com>
2026-02-09 17:00:20 -0800

    Add: published_at column to Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_16_010125) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_10_002303) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 end
