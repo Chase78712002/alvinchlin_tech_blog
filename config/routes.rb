de6e363be29905990b80091f2d97b6fc7bfb6160
Alvin Lin <alvinch.lin@gmail.com>
2026-02-11 17:39:24 -0800

    Add: routes for the login/out to work with sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "home#index"
+  get "login" => "sessions#new", as: :login
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy", as: :logout
   resources :posts do
     get "drafts", on: :collection
   end
