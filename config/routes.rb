94bfed1720a6648ebcbf606de9566694b7f72551
Alvin Lin <alvinch.lin@gmail.com>
2026-02-12 16:28:53 -0800

    add root path route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   get "login" => "sessions#new", as: :login
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy", as: :logout
+
   resources :posts do
     get "drafts", on: :collection
   end
