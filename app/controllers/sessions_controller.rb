341f5c271ec3cb8d306c159ff24d81f79170d098
Alvin Lin <alvinch.lin@gmail.com>
2026-02-12 16:31:19 -0800

    WIP...Sessions controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if valid_credentials?
+      session[:admin_authenticated] = true
+      redirect_to root_path, notice: "Logged In Successfully"
+    else
+      redirect_to login_path, alert: "Incorrect Password, check again"
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to root_path, flash: "You Logged Out Successfully!"
+  end
+
+  private
+
+  def valid_credentials?
+    username = params[:username]
+    password = params[:password]
+
+    false if username.blank? || password.blank?
   end
 end
